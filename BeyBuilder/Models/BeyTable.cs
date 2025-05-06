213dd14306df3b4a231b3405e41ebde8fd9f321a
Caleb Campbell <38718983+MrChido@users.noreply.github.com>
2025-05-06 11:57:22 -0500

    Add files via upload

--- BeyBuilder/Models/BeyTable.cs
+++ BeyBuilder/Models/BeyTable.cs
@@ -1,5 +1,6 @@
 public class BeyTable
 {
+    [Key]//This tells EF Core this is the Primary Key
     public int BeyID { get; set; }
     public string BeyName { get; set; }
     public string EnergyLayer { get; set; }
